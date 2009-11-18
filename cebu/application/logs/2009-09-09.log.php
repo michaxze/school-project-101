b3d4f5bb15cdb805d288eaa2b9404094a6511852
michael gimena <michael@patient.local>
2009-11-18 13:59:07 +0800

    remove oldfiles

--- cebu/application/logs/2009-09-09.log.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2009-09-09 14:50:30 -07:00 --- error: Uncaught Kohana_404_Exception: The page you requested, images/navmenu2.png, could not be found. in file C:/wamp/www/cebu/system/core/Kohana.php on line 841
-2009-09-09 14:50:42 -07:00 --- error: Uncaught Kohana_404_Exception: The page you requested, images/navmenu2.png, could not be found. in file C:/wamp/www/cebu/system/core/Kohana.php on line 841
-2009-09-09 14:50:45 -07:00 --- error: Uncaught Kohana_404_Exception: The page you requested, images/navmenu2.png, could not be found. in file C:/wamp/www/cebu/system/core/Kohana.php on line 841
-2009-09-09 14:51:05 -07:00 --- error: Uncaught Kohana_404_Exception: The page you requested, images/navmenu2.png, could not be found. in file C:/wamp/www/cebu/system/core/Kohana.php on line 841
-2009-09-09 14:51:14 -07:00 --- error: Uncaught Kohana_404_Exception: The page you requested, images/navmenu2.png, could not be found. in file C:/wamp/www/cebu/system/core/Kohana.php on line 841
-2009-09-09 14:51:37 -07:00 --- error: Uncaught Kohana_404_Exception: The page you requested, images/navmenu2.png, could not be found. in file C:/wamp/www/cebu/system/core/Kohana.php on line 841
