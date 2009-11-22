2c3df2872f9ebc190a2e0c5c948dd664da40f9e2
Paul Villacorta <pwvillacorta@cebudirectories.com>
2009-11-23 00:30:26 +0800

    Adding category method

--- cebudir/application/controllers/listings.php
+++ cebudir/application/controllers/listings.php
@@ -8,16 +8,25 @@
  */
 class Listings_Controller extends Controller {
 	
+	var $lists;
+	var $cat;
+	
+	function __construct()
+	{
+		$this->lists = new Listings_Core;
+		$this->cat   = new Categories_Core;
+		
+		parent::__construct();
+	}
+	
 	function index()
 	{	
 	    // Fetching of Categories
-		$cat = new Categories_Core;
-		$categories = $cat->get_categories();
+		$categories = $this->cat->get_categories();
 	
 	    // Fetching of Latest Listing
-	    $list = new Listings_Core;
-	    $listings = $list->get_listings(15);
-	
+	    $listings = $this->lists->get_listings(15);
+		
 		$page = new View('cebudirectories/listings/index');
 		$page->title = 'Listings - Cebu Directories Online Cebu Directory of Cebu City';
 		$page->menu  = 'listing';
@@ -31,12 +40,10 @@ class Listings_Controller extends Controller {
 	function show($business)
 	{
 		$business_name = strtolower(str_replace('-',' ',$business));
-		$list = new Listings_Core;
-		$listing = $list->get_listing($business_name);
+		$listing = $this->lists->get_listing($business_name);
 		$listing = $listing[0];
 		// Fetching of Categories
-		$cat = new Categories_Core;
-		$categories = $cat->get_categories();
+		$categories = $this->cat->get_categories();
 		
 		$page = new View('cebudirectories/listings/index');
 		$page->title = ucwords(strtolower($listing['bus_name'])) . ' - Cebu Directories';
@@ -55,9 +62,33 @@ class Listings_Controller extends Controller {
 		$page->description	 = $listing['bus_description'];
 		
 		$page->render(true);
-		//echo '<pre>';
-		//print_r($listing);
-		//echo '</pre>';
 	}
 	
+	function category($category)
+	{
+		$cat_var = $this->cat->get_category_by_name($category);
+		
+		if(!empty($cat_var)) {
+			foreach($cat_var as $cat) {
+				$cids[] = $cat['cat_id'];	
+			}
+			
+			// Fetching of Categories
+			$categories = $this->cat->get_categories();
+			
+			// Fetching of Listings by Category
+			$listing = $this->lists->get_listings(15, $cids);
+			
+			$page = new View('cebudirectories/listings/index');
+			$page->title = $cat_var[0]['cat_name'] . ' - Cebu Directories Online Cebu Directory of Cebu City';
+			$page->menu  = 'listing';
+			
+			$page->categories = $categories;
+			$page->listings   = $listing;
+			
+			$page->render(true);
+		}
+		
+		
+	}
 } // End Home Controller
