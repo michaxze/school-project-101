a0114d7478965b8587f11e1cf5c877888451220c
Paul Villacorta <pwvillacorta@cebudirectories.com>
2009-11-22 16:37:51 +0800

    Updating home controller

--- cebudir/application/controllers/home.php
+++ cebudir/application/controllers/home.php
@@ -6,36 +6,67 @@
  * @author     Paul Villacorta - Cebu Directories
  * @copyright  (c) 2006-2009 Cebu Directories
  */
-class Home_Controller extends Template_Controller {
-
-	// Set the name of the template to use
-	public $template = 'cebudirectories/home';
+class Home_Controller extends Controller {
 	
-	public function index()
+	function index()
 	{
-	    $this->template->title = 'Cebu Directories Online Cebu Directory of Cebu City';
+	    //$this->template->title = 'Cebu Directories Online Cebu Directory of Cebu City';
 	
 	    // Fetching of Categories
 	    $cat = new Categories_Core;
 	    $categories = $cat->get_categories();
-	    $this->template->categories = $categories;
+	    //$this->template->categories = $categories;
 	
 	    // Fetching of Latest Listing
 	    $list = new Listings_Core;
 	    $listings = $list->get_listings(5);
-	    $this->template->listings = $listings;
+	    //$this->template->listings = $listings;
 	
+		$page = new View('cebudirectories/home');
+		$page->title = 'Cebu Directories Online Cebu Directory of Cebu City';
+		
+		$page->categories = $categories;
+		$page->listings   = $listings;
+		
+		$page->render(true);
 	    // Fetching of Advertisers
 	    // Fetching of Featured Listing
 	    // Fetching of Banner
 	    // too many fetching goes in this area
 	}
 	
-	public function advertise()
+	function advertise()
 	{
-	    //echo "Pass Here i should be at advertise";	
-	    //$page = new View('cebudirectories/home');
-	    $this->template->title = 'Advertise - Cebu Directories Online Cebu Directory of Cebu City';
+	    $page = new View('cebudirectories/page_advertise');
+	    $page->title = 'Advertise - Cebu Directories Online Cebu Directory of Cebu City';
+		$page->menu  = 'advertise';
+		$page->render(true);
+	}
+	
+	function contactus()
+	{
+	    $page = new View('cebudirectories/page_contactus');
+	    $page->title = 'Contact Us - Cebu Directories Online Cebu Directory of Cebu City';
+		$page->menu  = 'contact';
+		$page->ctypes = array('Listing Inquiry',
+							  'Event Inquiry',
+							  'Advertising Inquiry',
+							  'Feedbacks and Comments - Thank us',
+							  'Other concerns');
+		$page->render(true);
+	}
+	
+	function services()
+	{
+		$page = new View('cebudirectories/page_services');
+	    $page->title = 'Services - Cebu Directories Online Cebu Directory of Cebu City';
+		$page->menu  = 'services';
+		$page->ctypes = array('Listing Inquiry',
+							  'Event Inquiry',
+							  'Advertising Inquiry',
+							  'Feedbacks and Comments - Thank us',
+							  'Other concerns');
+		$page->render(true);
 	}
 	
 } // End Home Controller
