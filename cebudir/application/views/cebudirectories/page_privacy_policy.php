433be59ca38a981554de1234e14b2fa03c4871b6
Paul Villacorta <pwvillacorta@cebudirectories.com>
2009-11-26 02:50:15 +0800

    adding pages for privacy and policy and terms of use

---
+++ cebudir/application/views/cebudirectories/page_privacy_policy.php
@@ -0,0 +1,32 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+<?php require_once('header.php'); ?>    
+
+    <div id="cd-content">
+    	<h1>Privacy Policy</h1>
+    	<p>
+    	Cebu Directories takes your privacy seriously.  This policy covers how 
+        Cebu Directories treats personal and business information that is collected 
+        via Cebu Directories.  This personal information includes name, address, 
+        telephone number, mobile number and email address when you register to 
+        Cebu Directories.  As well as business or what we labeled as listing informations 
+        such as name, address, telephone number, fax number, mobile number, website, and 
+        email. Names, email addresses and websites collected under the listings comments 
+        are treated as well.
+    	</p>
+    	
+        <p>
+    	Cebu Directories automatically receives and records information on our server logs 
+        from your browser, including your IP address.  Cebu Directories does not rent, sell, 
+        or share personal information about you with other people, company or organizations.
+    	</p>
+        
+        <p>
+    	Cebu Directories reserves the right to send you certain communications relating to 
+        Cebu Directories, such as service announcements and administrative messages.
+		We will notify you about significant changes in the way we treat personal and business 
+		information by sending a notice to the primary email address.  Id you have questions or 
+		suggestions, please complete a feedback form or you can contact us at info@cebudirectories.com
+    	</p>
+    </div>
+
+<?php require_once('footer.php'); ?>
---
+++ cebudir/application/views/cebudirectories/page_terms_of_use.php
@@ -0,0 +1,23 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+<?php require_once('header.php'); ?>    
+
+    <div id="cd-content">
+    	<h1>Terms of Use</h1>
+    	<p>
+    	Cebu Directories welcomes and values your listing contributions to Cebu 
+        Directories Listings.  We encourage you to communicate freely and if you 
+        would like to send feedback directly, please use our forms on Contact Us 
+        page or email us at info@cebudirectories.com.
+    	</p>
+    	
+        <p>
+    	To make sure that the information posted upon registration is informative, 
+        useful and correct, we ask everyone who registers to review and agree to 
+        the basic rules set out in this policy.  You will be receiving a system 
+        generated email from us requiring you to activate your application.  This 
+        process helps us in validating the information you posted and for your 
+        listing to appear on our Listing.
+    	</p>        
+    </div>
+
+<?php require_once('footer.php'); ?>
