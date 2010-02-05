bf4b2d4e392ae130187c5e21a5219cacccfd91b2
Paul Villacorta <pwvillacorta@cebudirectories.com>
2010-02-05 08:22:12 +0800

    changes for the new images to reflect, and ads section

--- cebudir/application/views/cebudirectories/advertising.php
+++ cebudir/application/views/cebudirectories/advertising.php
@@ -1,25 +1,33 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 
-<div id="ads">
-	<div class="header-top center title">Advertisement #1</div>
-	<ul>
-    	<li>Link Ad 1</li>
-        <li>Link Ad 2</li>
-        <li>Link Ad 3</li>
-        <li>Link Ad 4</li>
-    </ul>
-	<div class="header-bottom"></div>
+<div id="ads" style="border: none;">
+    <h5 style="text-align:left; padding: 0px; margin-bottom: 3px;"><a href="" style="border-bottom: 1px dotted #CCCCCC;">This is a Text Link Ads</a></h5>
+    <span>To find people you know who are already 
+    using Facebook, check out the Friend Finder.</span>
+    <div style="background: url(images/line.png) bottom repeat-x; height:5px; margin-bottom: 10px;"></div>
+    
+    <h5 style="text-align:left; padding: 0px; margin-bottom: 3px;"><a href="" style="border-bottom: 1px dotted #CCCCCC;">This is a Text Link Ads</a></h5>
+    <span>To find people you know who are already 
+    using Facebook, check out the Friend Finder.</span>
+    <div style="background: url(images/line.png) bottom repeat-x; height:5px; margin-bottom: 10px;"></div>
+    
+    <h5 style="text-align:left; padding: 0px; margin-bottom: 3px;"><a href="" style="border-bottom: 1px dotted #CCCCCC;">This is a Text Link Ads</a></h5>
+    <span>To find people you know who are already 
+    using Facebook, check out the Friend Finder.</span>
+    <div style="background: url(images/line.png) bottom repeat-x; height:5px; margin-bottom: 10px;"></div>
 </div>
 
-<div id="ads">
-	<div class="header-top center title">Advertisement #2</div>
-	<ul>
-    	<li>Link Ad 1</li>
-        <li>Link Ad 2</li>
-        <li>Link Ad 3</li>
-        <li>Link Ad 4</li>
-    </ul>
-	<div class="header-bottom"></div>
+<div id="ads" style="border: none;">
+    <h5 style="text-align:left; padding: 0px; margin-bottom: 3px;"><a href="" style="border-bottom: 1px dotted #CCCCCC;">This is a Text Link Ads</a></h5>
+    <span><img src="images/ads-sample.jpg" align="left" style="margin: 0px 5px 5px 0px;"/>To find people you know who are already 
+    using Facebook, check out the Friend Finder.</span>
+    <div style="background: url(images/line.png) bottom repeat-x; height:5px; margin-bottom: 10px;"></div>
+    
+    <h5 style="text-align:left; padding: 0px; margin-bottom: 3px;"><a href="" style="border-bottom: 1px dotted #CCCCCC;">This is a Text Link Ads</a></h5>
+    <span><img src="images/ads-sample.jpg" align="left" style="margin: 0px 5px 5px 0px;"/>To find people you know who are already 
+    using Facebook, check out the Friend Finder.</span>
+    <div style="background: url(images/line.png) bottom repeat-x; height:5px; margin-bottom: 10px;"></div>
+    
 </div>
 
 <div id="ads">
