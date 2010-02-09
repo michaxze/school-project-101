86ced0085e344ed7a4953520be6f804ab2fdb3a1
Paul Villacorta <pwvillacorta@cebudirectories.com>
2010-02-09 08:36:25 +0800

    Modified Services Page

--- cebudir/application/views/cebudirectories/page_services.php
+++ cebudir/application/views/cebudirectories/page_services.php
@@ -3,47 +3,54 @@
 
     <div id="cd-content">
     	<h1>Cebu Directories Services</h1>
-    	<p class="medium">
-    	<a href="<?php echo url::base(FALSE); ?>" title="Cebu Directories Online Cebu Directory" 
-        class="medium">Cebu Directories</a> is the next leading Cebu Directory site for 
-    	Cebu Businesses, Establishments and Talents. We provide comprehensive 
-    	contents, resources for small, medium and large companies.
-    	</p>
-    	
-    	<h3>Business Listing</h3>
-        <p class="medium">
-    	<a href="<?php echo url::base(FALSE); ?>" title="Cebu Directories Online Cebu Directory" 
-        class="medium">Cebu Directories</a> is the next leading Cebu Directory site for 
-    	Cebu Businesses, Establishments and Talents. We provide comprehensive 
-    	contents, resources for small, medium and large companies.
-    	</p>
         
-    	<h3>Web Design and Web Development</h3>
-        <p class="medium">
-    	<a href="<?php echo url::base(FALSE); ?>" title="Cebu Directories Online Cebu Directory" 
-        class="medium">Cebu Directories</a> also offers website development from simplest static 
-        single page to the most complex or dynamic website.  It may be a personal 
-        website, a business website or an internet application such as e-commerce 
-        setup, content management setup or a fully customized website.
-		</p>
-        <p class="medium">
-		Having a website exclusively enhance your customer's expression towards your 
-        business. A website acts as an essential step towards better business sensibilities.
+        <div class="left center services_block">
+        	<h3>Business Listing</h3>
+            <div class="services_listing"><a href="">
+            	<img class="icenter" src="images/services/business-directory-listing-off.jpg" />
+            </a></div>
+            
+            <p class="medium">
+    	Business directories benefit small, local companies 
+        as well as large, international companies. The important thing is that you get your name 
+        out there so customers know they can turn to your business. Online business directories 
+        are an inexpensive, effective way to maintain your business's standing
     	</p>
-    	        
-    	<h3>Advertising</h3>
-        <p class="medium">
-    	Cebu Directories is the next leading Cebu Directory site for 
-    	Cebu Businesses, Establishments and Talents. We provide comprehensive 
-    	contents, resources for small, medium and large companies.
-    	</p>
+        </div>
         
-        <h3>Domain Name and Web Hosting</h3>
-        <p class="medium">
-    	Cebu Directories is the next leading Cebu Directory site for 
-    	Cebu Businesses, Establishments and Talents. We provide comprehensive 
-    	contents, resources for small, medium and large companies.
+        <div class="left center services_block">
+        	<h3>Advertising</h3>
+            <div class="services_advertising"><a href="">
+	            <img class="icenter" src="images/services/advertising-off.jpg" />
+            </a></div>
+            
+            <p class="medium">
+    	Business directories benefit small, local companies 
+        as well as large, international companies. The important thing is that you get your name 
+        out there so customers know they can turn to your business. Online business directories 
+        are an inexpensive, effective way to maintain your business's standing
     	</p>
+        </div>
+        
+        <div class="left center services_block">
+        	<h3>Web Design and Development</h3>
+            <div class="services_webdev"><a href="">
+            	<img class="icenter" src="images/services/web-development-off.jpg" />
+            </a></div>
+            
+            <p class="medium">
+    	Business directories benefit small, local companies 
+        as well as large, international companies. The important thing is that you get your name 
+        out there so customers know they can turn to your business. Online business directories 
+        are an inexpensive, effective way to maintain your business's standing
+    	</p>
+        </div>
+        
+        
+        <div class="clear"></div>
+        
+        
+ 
         
     </div>
 
