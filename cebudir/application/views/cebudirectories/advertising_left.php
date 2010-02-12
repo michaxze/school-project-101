81b38f7013635dd1205fe945b64a0e97155a5be9
Paul Villacorta <pwvillacorta@cebudirectories.com>
2010-02-12 09:51:58 +0800

    adding new ads files

---
+++ cebudir/application/views/cebudirectories/advertising_left.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+
+<h5 style="padding-left: 0px;">Sponsored</h5>
+
+<div id="ads">
+	<img src="<?php echo url::base(); ?>images/advertising/ads-200-by-400.png" class="banner400" />
+
+</div>
---
+++ cebudir/application/views/cebudirectories/advertising_prime.php
@@ -0,0 +1,4 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+
+<div id="cd-banner"><img src="<?php echo url::base(); ?>images/banners/hostfortes-web-hosting.jpg" /></div>
+<div class="shadow"></div>
