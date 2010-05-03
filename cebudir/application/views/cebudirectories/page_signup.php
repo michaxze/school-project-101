8a3ae5a0a1df62257d88dbfb15d9d6426d53692e
Paul Villacorta <pwvillacorta@cebudirectories.com>
2010-05-03 14:28:03 +0800

    updated style for signup page

--- cebudir/application/views/cebudirectories/page_signup.php
+++ cebudir/application/views/cebudirectories/page_signup.php
@@ -1,7 +1,30 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 <?php require_once('header.php'); ?>
-
-
+	<script type="text/javascript">
+        $(document).ready(function(){
+            $("#form-signup input[type=text]").focus(function(){
+                //$(this).parent().find(".error").css("display", "none");
+				id = this.id;
+				if(id == "telno_areacode" || id == "telno" || id == "faxno_areacode"
+				   	|| id == "faxno" || id == "mobile_network" || id == "mobileno" ) { 
+					$(this).parent().find(".info").css("margin-left", "210px");
+				}
+				$(this).parent().find(".info").animate({opacity: 'toggle'}, 250);
+            }).blur(function(){
+				$(this).parent().find(".info").animate({opacity: 'hide'}, 450);
+            });
+			
+			$("#form-signup textarea").focus(function(){
+                $(this).parent().find(".error").css("display", "none");
+				$(this).parent().find(".info").animate({opacity: 'toggle'}, 200);
+				$(this).parent().find(".info").css("margin-top", "-148px");
+            }).blur(function(){
+				$(this).parent().find(".info").animate({opacity: 'hide'}, 450);
+            });
+        });
+        
+      
+    </script>
     <div id="cd-content">
       <?php if ($submitted) { ?>
         <h1>You have successfully signed up. Thank you for signing up.</h1>
@@ -31,15 +54,21 @@
             </tr>
             <tr>
               <td width="100"><?php print form::label('business_name', 'Business Name' );?> </td>
-              <td ><?php print form::input('business_name') . $req;?></td>
+              <td ><?php print form::input('business_name') . $req;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>
+              </td>
             </tr>
             <tr>
               <td ><?php print form::label('business_desc', 'Business Description' );?> </td>
-              <td ><?php print form::textarea('business_desc') . $req;?></td>
+              <td ><?php print form::textarea('business_desc') . $req;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>
+              </td>
             </tr>
             <tr>
               <td><?php print form::label('business_street', 'Street / Barangay' );?></td>
-              <td><?php print form::input('business_street') . $req;?></td>
+              <td><?php print form::input('business_street') . $req;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>
+              </td>
             </tr>
             <tr>
               <td><?php print form::label('business_area', 'Area' );?></td>
@@ -52,44 +81,63 @@
             <tr>
               <td><?php print form::label('telno', 'Telephone No.' );?></td>
               <td><span style="font-size:16px; padding-right:6px;">+63</span><?php print form::input('telno_areacode','',"maxlength=3 style=\"width:35px\"");?>
-                <?php print form::input('telno','',"maxlength=7 style=\"width:75px;\"") ;?></td>
+                <?php print form::input('telno','',"maxlength=7 style=\"width:75px;\"") ;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>  
+              </td>
             </tr>
             <tr>
               <td><?php print form::label('faxno', 'Fax No.' );?></td>
               <td><span style="font-size:16px; padding-right:6px;">+63</span><?php print form::input('faxno_areacode','',"maxlength=3 style=\"width:35px\"");?>
-                <?php print form::input('faxno','',"maxlength=7 style=\"width:75px;\"") ;?></td>
+                <?php print form::input('faxno','',"maxlength=7 style=\"width:75px;\"") ;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>  
+              </td>
             </tr>
             <tr>
               <td><?php print form::label('mobileno', 'Mobile No.' );?></td>
               <td><span style="font-size:16px; padding-right:6px;">+63</span><?php print form::input('mobileno_network','',"maxlength=3 style=\"width:35px\"");?>
-                <?php print form::input('mobileno','',"maxlength=7 style=\"width:75px;\"") ;?></td>
+                <?php print form::input('mobileno','',"maxlength=7 style=\"width:75px;\"") ;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>  
+              </td>
             </tr>
             <tr>
               <td><?php print form::label('email', 'Email Address' );?></td>
-              <td><?php print form::input('email') . $req;?></td>
+              <td><?php print form::input('email') . $req;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>
+              </td>
             </tr>
             <tr>
               <td><?php print form::label('website', 'Website' );?></td>
-              <td><?php print form::input('website') ;?></td>
+              <td><?php print form::input('website') ;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>
+              </td>
             </tr>
+            <!-- Social Media Section -->
             <tr>
               <td colspan="2"><h3 style="color: #6D8309;"><img src="<?php echo url::base(); ?>images/signup-two.png" align="absmiddle"/> Social Media Links</h3></td>
             </tr>
             <tr>
               <td><?php print form::label('twitter_link', 'Twitter Link' );?></td>
-              <td><?php print form::input('twitter_link') ;?></td>
+              <td><?php print form::input('twitter_link') ;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>
+              </td>
             </tr>
             <tr>
               <td><?php print form::label('facebook_link', 'Facebook Link' );?></td>
-              <td><?php print form::input('facebook_link') ;?></td>
+              <td><?php print form::input('facebook_link') ;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>
+              </td>
             </tr>
             <tr>
               <td><?php print form::label('yahoo_id', 'Yahoo ID' );?></td>
-              <td><?php print form::input('yahoo_id') ;?></td>
+              <td><?php print form::input('yahoo_id') ;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>
+              </td>
             </tr>
             <tr>
               <td><?php print form::label('skype_id', 'Skype ID' );?></td>
-              <td><?php print form::input('skype_id') ;?></td>
+              <td><?php print form::input('skype_id') ;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>
+              </td>
             </tr>
             <tr>
               <td colspan="2"><h3 style="color: #6D8309;"><img src="<?php echo url::base(); ?>images/signup-three.png" align="absmiddle"/> Billing Information</h3></td>
@@ -99,16 +147,20 @@
               <td><?php print form::dropdown('area',$provinces, 'standard');?></td>
             </tr>
             <tr>
-              <td><?php print form::label('facebook_link', 'Facebook Link' );?></td>
-              <td><?php print form::input('facebook_link') ;?></td>
+              <td></td>
+              <td>&nbsp;<img src="<?php echo url::base(); ?>images/payment-options.jpg" /></td>
             </tr>
             <tr>
-              <td><?php print form::label('yahoo_id', 'Yahoo ID' );?></td>
-              <td><?php print form::input('yahoo_id') ;?></td>
+              <td><?php print form::label('billing_name', 'Billing Name' );?></td>
+              <td><?php print form::input('billing_name') ;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>
+              </td>
             </tr>
             <tr>
-              <td><?php print form::label('skype_id', 'Skype ID' );?></td>
-              <td><?php print form::input('skype_id') ;?></td>
+              <td><?php print form::label('billing_address', 'Billing Address' );?></td>
+              <td><?php print form::input('billing_address') ;?>
+              		<div class="help-tooltip info">Minimum 4 characters, maximum 15 characters. This is your handle on Digg so don't make it too personal.</div>
+              </td>
             </tr>
             <tr>
               <td></td>
@@ -116,8 +168,9 @@
             </tr>
           </table>
           <div class="left" style="width: 320px; margin-left: 10px;">
-          		<img src="<?php echo url::base(); ?>images/signup-premium-account.png"/>
-          		<img src="<?php echo url::base(); ?>images/signup-benefits.png"/>
+          		<img src="<?php echo url::base(); ?>images/signup-banner.png"/><br /><br />
+                <img src="<?php echo url::base(); ?>images/signup-benefits.png"/>
+                
           </div>
           <div class="clear"></div>
       </div>
