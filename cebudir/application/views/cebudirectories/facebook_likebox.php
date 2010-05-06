d8a85e52dbce6fc8caf09424bab0f101fea89da0
Paul Villacorta <pwvillacorta@cebudirectories.com>
2010-05-06 23:38:16 +0800

    adding facebook likebox

---
+++ cebudir/application/views/cebudirectories/facebook_likebox.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+
+<iframe src="http://www.facebook.com/plugins/likebox.php?profile_id=281222139431&amp;width=200&amp;stream=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:200px;" allowTransparency="true"></iframe>
