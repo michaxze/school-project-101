e32c06b5418cbe777d5a5ced9722271a860ed0dd
Paul Villacorta <pwvillacorta@cebudirectories.com>
2010-06-06 11:48:59 -0500

    add gem_path in environment

--- ads/config/environment.rb
+++ ads/config/environment.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file
 
+ENV['GEM_PATH'] = '/home/michael/ruby/gems:/usr/lib/ruby/gems/1.8'
+
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
@@ -20,8 +22,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-#  config.gem "will_paginate"
-
+#  config.gem "pb_will_paginate" #, :version => '2.3.14'
+  config.gem 'will_paginate'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -42,3 +44,5 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+#require "will_paginate"
